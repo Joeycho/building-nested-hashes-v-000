09f108c2be63fb211a5603b167e56253532963b7
Joey Cho <cjy12182000@naver.com>
2018-06-04 14:42:51 +0000

    automatically backed up by learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def bonus
   epic_tragedy = {
    :montague => {
@@ -21,6 +23,7 @@ def bonus
   }
 
   #code your solution here:
+  binding.pry
 epic_tragedy[:motague][:hero][:status]="dead"
 epic_tragedy[:capulet][:heroine][:status]="dead"
 
